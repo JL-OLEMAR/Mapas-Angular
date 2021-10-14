371bb63fad98ee08a750a8f6c0deb78f7f34650b
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-14 11:30:28 -0500

    Obtener las coordenas centrales del mapa

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core'
 import * as mapboxgl from 'mapbox-gl'
 
 @Component({
@@ -6,10 +6,12 @@ import * as mapboxgl from 'mapbox-gl'
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef
   mapa!: mapboxgl.Map
   zoomLevel: number = 10
+  // longitud, latitud
+  center: [number, number] = [-77.02952900833222, -12.044404909131146]
 
   // constructor () { }
 
@@ -17,18 +19,36 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      // longitud, latitud
-      center: [-77.02952900833222, -12.044404909131146],
+      center: this.center,
       zoom: this.zoomLevel
     })
 
+    // zoom del mapa controlado
     this.mapa.on('zoom', () => {
       this.zoomLevel = this.mapa.getZoom()
     })
 
     this.mapa.on('zoomend', () => {
-      if (this.mapa.getZoom() >= 18) { this.mapa.zoomTo(18) }
+      if (this.mapa.getZoom() > 18) { this.mapa.zoomTo(18) }
     })
+
+    // Coordenadas del mapa controladas
+    this.mapa.on('move', (e) => {
+      const target = e.target
+      const { lng, lat } = target.getCenter()
+      this.center = [lng, lat]
+    })
+  }
+
+  ngOnDestroy (): void {
+    // destruir el listener zoom
+    this.mapa.off('zoom', () => {})
+
+    // destruir el listener zoomend
+    this.mapa.off('zoomend', () => {})
+
+    // destruir el listener move
+    this.mapa.off('move', () => {})
   }
 
   zoomOut (): void {
