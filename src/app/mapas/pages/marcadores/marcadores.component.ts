dfab2cb5c0a10035902938ca9ee692ba64d08629
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-14 12:16:18 -0500

    Add marcador de forma dinamica

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styles: []
+  styleUrls: ['./marcadores.component.css']
 })
-export class MarcadoresComponent implements OnInit {
-  // constructor () { }
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef
+  mapa!: mapboxgl.Map
+  zoomLevel: number = 16
+  center: [number, number] = [-77.02952900833222, -12.044404909131146]
 
-  ngOnInit (): void {
+  ngAfterViewInit (): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    })
+
+    // Marker personalizado
+    // const markerHtml: HTMLElement = document.createElement('div')
+    // markerHtml.innerHTML = '🚩 here'
+    // new mapboxgl.Marker({ element: markerHtml })
+    //   .setLngLat(this.center)
+    //   .addTo(this.mapa)
+  }
+
+  agregarMarcador (): void {
+    // color hexadecimal random
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16))
+
+    const nuevoMarcador = new mapboxgl.Marker({ draggable: true, color })
+      .setLngLat(this.center)
+      .addTo(this.mapa)
+  }
+
+  irMarcador (): void {
+
   }
 }
