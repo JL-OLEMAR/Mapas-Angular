845ae04c08ec2f4a6c8df69defa4988e1ec263b6
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-15 21:46:17 -0500

    Pagina de propiedades

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core'
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styles: []
+  styles: [`
+    div {
+      height: 150px;
+      margin: 0;
+      width: 100%;
+    }
+  `]
 })
-export class MiniMapaComponent implements OnInit {
-  // constructor () { }
+export class MiniMapaComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0]
+  @ViewChild('mapa') divMapa!: ElementRef
 
-  ngOnInit (): void {
+  ngAfterViewInit (): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    })
+
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(mapa)
   }
 }
